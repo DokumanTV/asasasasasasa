0b9e925e577d0b8135e241476f98bff920a2ff7f
Glitch (stark-code-bos-altyapi) <none>
2020-04-10 21:10:01 +0000

    🏪🍉 Checkpoint ./komutlar/tagyaz.js:10620326/1744

--- komutlar/tagyaz.js
+++ komutlar/tagyaz.js
@@ -1,6 +1,22 @@
-exports.run = (client,message) => {
-  
-  message.channel.send(`∻ Aramıza Hoş Geldin** ${client.emojies.get("698201060271194165")}`)
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-} 
+exports.run = (client, message) => {
+   message.channel.send('``∻`` **Ailemize Hoş Geldin** :star:').then(message => {
 
+ });
+  }
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['balık', 'balıktut', 'balık-tut'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'tag',
+  description: 'Tagı Atar.',
+  usage: 'tag'
+};
+   
