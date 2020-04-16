a0c64be9b3f40379aa9edec85fc12499d42049df
Glitch (stark-code-bos-altyapi) <none>
2020-04-16 08:24:30 +0000

    ⭐️😼 Checkpoint ./komutlar/sessay.js:10620326/1536

---
+++ komutlar/sessay.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (client, message, args) => {
+  if(message.author.bot || message.channel.type === "dm") return;
+  const voiceChannels = message.guild.channels.filter(c => c.type === 'voice');
+    let count = 0;
+    
+    for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
+      const emoji = client.emojis.find(emoji => emoji.name === "♱");
+  const arrifentembed = new Discord.RichEmbed()
+  .setColor("black")
+  .setAuthor('Bilgi', `${message.author.displayAvatarURL}`)
+        .addField(`♱ Ses kanallarında ${count} kişi bulunmaktadır.`, `♱ Sunucuda ${message.guild.memberCount} kişi bulunmaktadır.`)
+ 
+  message.channel.sendEmbed(arrifentembed)
+  message.react(emoji)
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'sessay',
+  description: 'kullanıcıyı susturur.',
+  usage: 'sessay'
+};
