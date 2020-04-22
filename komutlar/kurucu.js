63f58386b40a259635f13fa266b0c502be9a3c6c
Glitch (stark-code-bos-altyapi) <none>
2020-04-22 19:21:38 +0000

    🖖🏍 Checkpoint ./komutlar/sayma2.js:10620326/2735 ./komutlar/kurucu.js:10620326/89

--- komutlar/kurucu.js
+++ komutlar/kurucu.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 exports.run = (client, message) => {
-   message.channel.send('<a:elysiontac:699938882761785354> <@533650583215800320> Sunucu Sahibi').then(message => {
+   message.channel.send('<a:dcshine:701865076050755675> <@533650583215800320> Sunucu Sahibi').then(message => {
 
  });
   }
