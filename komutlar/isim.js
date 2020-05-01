b851ebd27198e191059c770bfe2877f09250e36b
Glitch (stark-code-bos-altyapi) <none>
2020-05-01 14:47:07 +0000

    👀🙏 Checkpoint ./komutlar/erkek.js:10620326/6505 ./komutlar/isim.js:10620326/2578 ./komutlar/yazbot.js:10620326/471

--- komutlar/isim.js
+++ komutlar/isim.js
@@ -1,24 +1,24 @@
 const Discord = require("discord.js");
 const db = require('quick.db');
 exports.run = (client, message, args) => {
-  const kayıtlı = message.guild.roles.find(r => r.id === "705785404586065941"); //buraya erkek rolünüzün id'sini koyun
-  const male = message.guild.roles.find(r => r.id === "705785405169205288"); //buraya erkek rolünüzün id'sini koyun
-  const misafir = message.guild.roles.find(r => r.id === "705787134623088760"); //buraya misafir rolünüzün id'sini koyun.
   const log = message.guild.channels.find(c => c.id === "705783164366225480"); //buraya kayıt log id koyun
-  const dogrulandi = client.emojis.find(emoji => emoji.name === "space");
+  const tag = "★";//YAZMAK İSTERSENİZ TAGINIZ ( BOŞ BIRAKABİLİRSİNİZ )
+  const dogrulandi = client.emojis.find(emoji => emoji.name === "white_check_mark");
   if(!message.member.roles.array().filter(r => r.id === "705785405550755902")[0]) { //buraya kayıt sorumlusu rolünün id'sini giriniz. SUNUCU AYARLARINDAN kopyalayın.
     return message.channel.send("Bu işlemi sadece Ayarlanmış Kayıt Sorumluları gerçekleştirebilir.");
   } else {
     let member = message.mentions.users.first() || client.users.get(args.join(' '))
       if(!member) return message.channel.send("Bir kullanıcı girin.")
     const c = message.guild.member(member)
-    c.addRole(kayıtlı)
-    c.addRole(male)
-    c.removeRole(misafir)
+    const nick = args[1];
+    const yas = args[2];
+      if(!nick) return message.channel.send("Bir isim girin.")
+      if(!yas) return message.channel.send("Bir yaş girin.")
+    c.setNickname(`${tag} ${nick} | ${yas}`)
     const embed = new Discord.RichEmbed()
-    .setThumbnail("https://cdn.discordapp.com/attachments/704492532427522128/705125025233240171/ezgif.com-crop_13.gif")
-    .setDescription(` **${message.author.tag}** adlı yetkili **${c.user.tag}** kişiye erkek rolü verildi. !`)
+    .setDescription(`**${c.user.tag}** kişinin yeni adı **${tag} ${nick} , ${yas} !**`)
     .setColor("GREEN")
+        .setFooter(client.user.username, message.guild.iconURL);
     log.send(embed)
     message.react(dogrulandi)
   }
@@ -30,7 +30,7 @@ exports.conf = {
   permLevel: 0
 };
 exports.help = {
-  name: "erkek",
+  name: "nick",
   description: "",
   usage: ""
 };
