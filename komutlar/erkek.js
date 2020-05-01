5e67f25ba0cb4f631f307b38f6ddc1ae5ad8404a
Glitch (stark-code-bos-altyapi) <none>
2020-05-01 18:25:50 +0000

    🍞⛵️ Checkpoint ./komutlar/erkek.js:10620326/78

--- komutlar/erkek.js
+++ komutlar/erkek.js
@@ -16,7 +16,6 @@ exports.run = (client, message, args) => {
     c.addRole(male)
     c.removeRole(misafir)
     const embed = new Discord.RichEmbed()
-    .setThumbnail("https://media0.giphy.com/media/B4jfJqiIxvU08/giphy.gif")
     .setDescription(` **<@${c.user.id}>** adlı kişiye **<@&${kayıtlı.id}>** rolü verildi. !`)
   .setColor("0x020525")
     .setFooter(client.user.username, message.guild.iconURL);
