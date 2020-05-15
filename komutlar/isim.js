9cfba8305bc80a25d87cc6024a5db424e244e64d
Glitch (stark-code-bos-altyapi) <none>
2020-05-15 13:09:35 +0000

    👢🐊 Checkpoint ./komutlar/isim.js:10620326/2

--- komutlar/isim.js
+++ komutlar/isim.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const db = require('quick.db');
 exports.run = (client, message, args) => {
   const log = message.guild.channels.find(c => c.id === "709432692290093156"); //buraya kayıt log id koyun
-  const tag = "⤫";//YAZMAK İSTERSENİZ TAGINIZ ( BOŞ BIRAKABİLİRSİNİZ )
+  const tag = "亽";//YAZMAK İSTERSENİZ TAGINIZ ( BOŞ BIRAKABİLİRSİNİZ )
   const dogrulandi = client.emojis.find(emoji => emoji.name === "siyahtik");
   if(!message.member.roles.array().filter(r => r.id === "709432624753541181")[0]) { //buraya kayıt sorumlusu rolünün id'sini giriniz. SUNUCU AYARLARINDAN kopyalayın.
     return message.channel.send("Bu işlemi sadece Ayarlanmış Kayıt Sorumluları gerçekleştirebilir.");
