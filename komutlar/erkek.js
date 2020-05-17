4293dc83eae0b50c2669843f4bea65973c9e1c5d
Glitch (stark-code-bos-altyapi) <none>
2020-05-17 19:28:38 +0000

    🍘🏡 Checkpoint ./komutlar/erkek.js:10620326/330 ./komutlar/isim.js:10620326/122 ./komutlar/kadın.js:10620326/331

--- komutlar/erkek.js
+++ komutlar/erkek.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 const db = require('quick.db');
 exports.run = (client, message, args) => {
-  const kayıtlı = message.guild.roles.find(r => r.id === "709432638057873458"); //buraya erkek rolünüzün id'sini koyun
-  const male = message.guild.roles.find(r => r.id === "711210029461667911"); //buraya erkek rolünüzün id'sini koyun
-  const smale = message.guild.roles.find(r => r.id === "709432638678761574"); //buraya erkek rolünüzün id'sini koyun
-  const misafir = message.guild.roles.find(r => r.id === "709432640801079386"); //buraya misafir rolünüzün id'sini koyun.
-  const log = message.guild.channels.find(c => c.id === "709432692290093156"); //buraya kayıt log id koyun
-  const dogrulandi = client.emojis.find(emoji => emoji.name === "mavitik");
-  if(!message.member.roles.array().filter(r => r.id === "709432624753541181")[0]) { //buraya kayıt sorumlusu rolünün id'sini giriniz. SUNUCU AYARLARINDAN kopyalayın.
+  const kayıtlı = message.guild.roles.find(r => r.id === "711600933351718933"); //buraya erkek rolünüzün id'sini koyun
+  const male = message.guild.roles.find(r => r.id === "711659516231417958"); //buraya erkek rolünüzün id'sini koyun
+  const smale = message.guild.roles.find(r => r.id === "711600934207225956"); //buraya erkek rolünüzün id'sini koyun
+  const misafir = message.guild.roles.find(r => r.id === "711600935486488636"); //buraya misafir rolünüzün id'sini koyun.
+  const log = message.guild.channels.find(c => c.id === "711600980273135676"); //buraya kayıt log id koyun
+  const dogrulandi = client.emojis.find(emoji => emoji.name === "verify");
+  if(!message.member.roles.array().filter(r => r.id === "711600922542866532")[0]) { //buraya kayıt sorumlusu rolünün id'sini giriniz. SUNUCU AYARLARINDAN kopyalayın.
     return message.channel.send("Bu işlemi sadece Ayarlanmış Kayıt Sorumluları gerçekleştirebilir.");
   } else {
     let member = message.mentions.users.first() || client.users.get(args.join(' '))
@@ -18,7 +18,7 @@ exports.run = (client, message, args) => {
     c.addRole(smale)
     c.removeRole(misafir)
     const embed = new Discord.RichEmbed()
-    .setDescription(`<a:bluestar:710931362072363090> **<@${c.user.id}>** adlı kişiye **<@&${kayıtlı.id}>** rolü verildi. !`)
+    .setDescription(`<a:mavitik:711638776853561395> **<@${c.user.id}>** adlı kişiye **<@&${kayıtlı.id}>** rolü verildi. !`)
     .setColor("0x42fcd7")
     log.send(embed)
     message.react(dogrulandi)
--- komutlar/kadın.js
+++ komutlar/kadın.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 const db = require('quick.db');
 exports.run = (client, message, args) => {
-  const kayıtlı = message.guild.roles.find(r => r.id === "709432636472295455"); //buraya kadın rolünüzün id'sini koyun
-  const male = message.guild.roles.find(r => r.id === "711210032234102835"); //buraya kadın rolünüzün id'sini koyun
-  const smale = message.guild.roles.find(r => r.id === "709432637353230396"); //buraya kadın rolünüzün id'sini koyun
-  const misafir = message.guild.roles.find(r => r.id === "709432640801079386"); //buraya misafir rolünüzün id'sini koyun.
-  const log = message.guild.channels.find(c => c.id === "709432692290093156"); //buraya kayıt log id koyun
-  const dogrulandi = client.emojis.find(emoji => emoji.name === "pembetik");
-  if(!message.member.roles.array().filter(r => r.id === "709432624753541181")[0]) { //buraya kayıt sorumlusu rolünün id'sini giriniz. SUNUCU AYARLARINDAN kopyalayın.
+  const kayıtlı = message.guild.roles.find(r => r.id === "711600932139303072"); //buraya kadın rolünüzün id'sini koyun
+  const male = message.guild.roles.find(r => r.id === "711659514407157861"); //buraya kadın rolünüzün id'sini koyun
+  const smale = message.guild.roles.find(r => r.id === "711600932818780200"); //buraya kadın rolünüzün id'sini koyun
+  const misafir = message.guild.roles.find(r => r.id === "711600935486488636"); //buraya misafir rolünüzün id'sini koyun.
+  const log = message.guild.channels.find(c => c.id === "711600980273135676"); //buraya kayıt log id koyun
+  const dogrulandi = client.emojis.find(emoji => emoji.name === "verify");
+  if(!message.member.roles.array().filter(r => r.id === "711600922542866532")[0]) { //buraya kayıt sorumlusu rolünün id'sini giriniz. SUNUCU AYARLARINDAN kopyalayın.
     return message.channel.send("Bu işlemi sadece Ayarlanmış Kayıt Sorumluları gerçekleştirebilir.");
   } else {
     let member = message.mentions.users.first() || client.users.get(args.join(' '))
@@ -18,7 +18,7 @@ exports.run = (client, message, args) => {
     c.addRole(smale)
     c.removeRole(misafir)
     const embed = new Discord.RichEmbed()
-    .setDescription(`<a:redstar:710931008635142155> **<@${c.user.id}>** adlı kişiye **<@&${kayıtlı.id}>** rolü verildi. !`)
+    .setDescription(`<a:pembetik:711638772340752435> **<@${c.user.id}>** adlı kişiye **<@&${kayıtlı.id}>** rolü verildi. !`)
     .setColor("0xff5cf3")
     log.send(embed)
     message.react(dogrulandi)
