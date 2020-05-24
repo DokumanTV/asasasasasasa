ced08db92d1f85f5bccd2be78ab9d96a5e5e2484
Glitch (stark-code-bos-altyapi) <none>
2020-05-24 16:53:01 +0000

    🎅🍖 Checkpoint ./komutlar/cwduyuru.js:10620326/2

--- komutlar/cwduyuru.js
+++ komutlar/cwduyuru.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
 if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`Bu komutu kullanmak için **Yönetici** olmalısın.`);
 
   let mesaj = args.slice(0).join(' ');
-  let tag = "⚚"
+  let tag = "✭"
 
   if (!mesaj) return message.reply('Birşey Yazman Gerekiyor!');
 
