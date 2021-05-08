99253c822dd730734bf4eea9f65e60b891b0abe5
Glitch (dokumanbotfuny) <none>
2021-05-08 13:11:36 +0000

    🎭👍 Checkpoint ./komutlar/yardım-yazı.js:24387843/311

--- komutlar/yardım-yazı.js
+++ komutlar/yardım-yazı.js
@@ -10,18 +10,18 @@ const embed  = new Discord.MessageEmbed()
 .setColor('RANDOM')
 .setDescription(`
 
-  !!!! İşte Logo Yazı Yardım Komutları !!!!
+  🔴 İşte Logo Yazı Yardım Komutları 🔴
  
 
-\`${p}yazı-cool Yazı\` - Cool Logo Yazı.
-\`${p}yazı-alev Yazı\` - Alevli Logo Yazı.
-\`${p}yazı-neon Yazı\` - Neon Logo Yazı.
-\`${p}yazı-anime Yazı\` - Anime Logo .
-\`${p}yazı-altın Yazı\` - Altın Logo Yazı.
-\`${p}yazı-dracula Yazı\` - Dracula Logo Yazı.
-\`${p}yazı-everest Yazı\` - Everest Logo Yazı.
+\**▪${p}yazı-cool Yazı\▪** - Cool Logo Yazı. 
+\**▪${p}yazı-alev Yazı\▪** - Alevli Logo Yazı.
+\**▪${p}yazı-neon Yazı\▪** - Neon Logo Yazı.
+\**▪${p}yazı-anime Yazı\▪** - Anime Logo .
+\**▪${p}yazı-altın Yazı\▪** - Altın Logo Yazı.
+\**▪${p}yazı-dracula Yazı\▪** - Dracula Logo Yazı.
+\**▪${p}yazı-everest Yazı\▪** - Everest Logo Yazı.
 `)
-.setFooter('"[Botu Ekle](https://discord.com/api/oauth2/authorize?client_id=791213780138852392&permissions=8&scope=bot")')
+.setFooter(`[Botu Ekle](link)`)
 .setTimestamp()
 
 if(!nicat) return message.channel.send(embed)
@@ -36,6 +36,6 @@ permLevel: 0
 }
 exports.help = {
 name: 'yardım-yazı',
-description: 'anticode nicat.dcw',
+description: 'yardım yazı',
 usage: 'Yazı Yardım'
 }
