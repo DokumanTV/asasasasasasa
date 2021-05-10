fd1e0954a7f92db7309502f5d2a085e16fe71ef9
Glitch (dokumanbotfuny) <none>
2021-05-10 08:52:41 +0000

    🏓😼 Checkpoint ./komutlar/yardımmenü.js:24387843/111

--- komutlar/yardımmenü.js
+++ komutlar/yardımmenü.js
@@ -6,14 +6,13 @@ const imdat = new wensamita.MessageEmbed()
 .setThumbnail(message.author.avatarURL({ dynamic: true }))
 .setColor('RANDOM')
 .setTitle(`🤖 » ${client.user.username} Yardım Menüsü`)
-.addField('\u200b', "[Botu Ekle!](https://discord.com/oauth2/authorize?client_id=791213780138852392&permissions=8&scope=bot)")//Botun Davet Linki
-.addField('\u200b', "[Abone Ol!](https://youtube.com/channel/UC9c6nECzH3N2tHELi1Bl47w)")//Youtube Kanalınız Varsa Link
+.addField('\u200b', "[Botu Ekle!](https://discord.com/oauth2/authorize?client_id=791213780138852392&permissions=8&scope=bot)") .addField('\u200b', "[Abone Ol!](https://youtube.com/channel/UC9c6nECzH3N2tHELi1Bl47w)")//Youtube Kanalınız Varsa Link
 .addField('🔥 » Logo Yazı Komutları', 'f!yardım-yazı')
 .addField('🔠  » Kelime Oyunu Komutları', 'f!yardım-kelime')
 .addField(':crossed_swords: » 2 Kişilik Oyunlar', 'f!yardım-2-kişilik')
 .addField(':game_die: » Eğlence Komutları', 'Yapım Aşaması') 
 .addField('🥊 » Kavga Komutları', 'Yapım Aşaması') //.addField('') Bunları çoğaltarak daha fazla satır ekleyebilirsiniz.
-.setDescription(`:link:・LİNKLER`)
+.setDescription(`:link:・**LİNKLER**`)
 .setImage('https://im2.ezgif.com/tmp/ezgif-2-782cd0087529.gif')
 .setTimestamp()
 message.channel.send(imdat)
