972ad5132e8ec7b6ae623984697b419d4f4a0ce1
Glitch (dokumanbotfuny) <none>
2021-05-10 08:07:58 +0000

    🌷🦄 Checkpoint ./komutlar/yardımmenü.js:24387843/965

--- komutlar/yardımmenü.js
+++ komutlar/yardımmenü.js
@@ -6,14 +6,15 @@ const imdat = new wensamita.MessageEmbed()
 .setThumbnail(message.author.avatarURL({ dynamic: true }))
 .setColor('RANDOM')
 .setTitle(`<:wow:836289529400918046> » ${client.user.username} Yardım Menüsü`)
-.addField('<:wow:836289529400918046> » Logo Yazı Komutları <:wow:836289529400918046>', 'f!yardım-yazı')
-.addField('<:wow:836289529400918046> » Kelime Oyunu Komutları <:wow:836289529400918046>', 'f!yardım-kelime')
-.addField('<:wow:836289529400918046> » 2 Kişilik Oyunlar <:wow:836289529400918046>', 'f!yardım-2-kişilik')
-.addField('<:wow:836289529400918046> » Eğlence Komutları <:wow:836289529400918046>', 'Yapım Aşaması') 
-.addField('<:wow:836289529400918046> » Nsfw Komutları <:wow:836289529400918046>', 'Yapım Aşaması') //.addField('') Bunları çoğaltarak daha fazla satır ekleyebilirsiniz.
+.addField('<:wow:836289529400918046> » Logo Yazı Komutları', 'f!yardım-yazı')
+.addField('<:wow:836289529400918046> » Kelime Oyunu Komutları', 'f!yardım-kelime')
+.addField('<:wow:836289529400918046> » 2 Kişilik Oyunlar', 'f!yardım-2-kişilik')
+.addField(':game_die: » Eğlence Komutları', 'Yapım Aşaması') 
+.addField('<:wow:836289529400918046> » Nsfw Komutları', 'Yapım Aşaması') //.addField('') Bunları çoğaltarak daha fazla satır ekleyebilirsiniz.
 .addField('\u200b', "[Botu Ekle!](https://discord.com/oauth2/authorize?client_id=791213780138852392&permissions=8&scope=bot)")//Botun Davet Linki
 .addField('\u200b', "[Abone Ol!](https://youtube.com/channel/UC9c6nECzH3N2tHELi1Bl47w)")//Youtube Kanalınız Varsa Link
-.addField("https://im2.ezgif.com/tmp/ezgif-2-782cd0087529.gif")//Gif Varsa Gifinizin Linki (Bunlardan Yoksa Silebilirsiniz)
+.setDescription('https://im2.ezgif.com/tmp/ezgif-2-782cd0087529.gif')
+.setTimestamp()
 message.channel.send(imdat)
 };
 exports.conf = {
