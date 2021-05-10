72724d677d08cdf4ef0f9301d2c2b8bcb1b10fe6
Glitch (dokumanbotfuny) <none>
2021-05-10 08:30:05 +0000

    👵🚉 Checkpoint ./komutlar/yardımmenü.js:24387843/9238

--- komutlar/yardımmenü.js
+++ komutlar/yardımmenü.js
@@ -6,14 +6,14 @@ const imdat = new wensamita.MessageEmbed()
 .setThumbnail(message.author.avatarURL({ dynamic: true }))
 .setColor('RANDOM')
 .setTitle(`🤖 » ${client.user.username} Yardım Menüsü`)
-.addField('🔥 » Logo Yazı Komutları', 'f!yardım-yazı')
-.addField('🔠  » Kelime Oyunu Komutları', 'f!yardım-kelime')
+.addField(':fire: » Logo Yazı Komutları', 'f!yardım-yazı')
+.addField(':capital_abcd:  » Kelime Oyunu Komutları', 'f!yardım-kelime')
 .addField(':crossed_swords: » 2 Kişilik Oyunlar', 'f!yardım-2-kişilik')
 .addField(':game_die: » Eğlence Komutları', 'Yapım Aşaması') 
-.addField('🥊 » Kavga Komutları', 'Yapım Aşaması') //.addField('') Bunları çoğaltarak daha fazla satır ekleyebilirsiniz.
-.addField('\u200b', "[Botu Ekle!](https://discord.com/oauth2/authorize?client_id=791213780138852392&permissions=8&scope=bot)")//Botun Davet Linki
-.addField('\u200b', "[Abone Ol!](https://youtube.com/channel/UC9c6nECzH3N2tHELi1Bl47w)")//Youtube Kanalınız Varsa Link
-.setDescription('https://im2.ezgif.com/tmp/ezgif-2-782cd0087529.gif')
+.addField(':boxing_glove: » Kavga Komutları', 'Yapım Aşaması') //.addField('') Bunları çoğaltarak daha fazla satır ekleyebilirsiniz.
+.setTitle(`:wincelink:・LİNKLER`)
+.addField('\u200b', "[Botu Ekle!](https://discord.com/oauth2/authorize?client_id=791213780138852392&permissions=8&scope=bot) ,"[Abone Ol!](https://youtube.com/channel/UC9c6nECzH3N2tHELi1Bl47w)")//Youtube Kanalınız Varsa Link ")
+.setImage('https://im2.ezgif.com/tmp/ezgif-2-782cd0087529.gif')
 .setTimestamp()
 message.channel.send(imdat)
 };
