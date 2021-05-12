b096e270ac9623c8bd7a45d19758ea9d064e4202
Glitch (dokumanbotfuny) <none>
2021-05-12 14:36:37 +0000

    ✋🎢 Checkpoint ./bot.js:25557806/1068 ./package.json:25557806/2090 ./komutlar/seviyesistemi.js:25557806/1393 ./komutlar/yardım-bilgi.js:25557806/520 ./komutlar/yılbasınanekadar.js:25557806/3800 ./komutlar/yapayzeka.js:25557806/616 ./komutlar/yardımmenü.js:25557806/771

--- bot.js
+++ bot.js
@@ -62,33 +62,6 @@ client.load = command => {
   });
 };
 
-//-------------------SEVİYE SİSTEMİ-------------------\\
-
-client.cooldown = new Discord.Collection();
-client.config = {
-cooldown: 1 * 1000
-}
-client.db = require("quick.db");
-client.on("message", async (message) => {
-    if (!message.guild || message.author.bot) return;
-    // XP
-    exp(message);
-function exp(message) {
-    if (!client.cooldown.has(`${message.author.id}`) || (Date.now() - client.cooldown.get(`${message.author.id}`) > client.config.cooldown)) {
-        let exp = client.db.add(`exp_${message.author.id}`, 1);
-        let level = Math.floor(0.3 * Math.sqrt(exp));
-        let lvl = client.db.get(`level_${message.author.id}`) || client.db.set(`level_${message.author.id}`,1);;
-        if (level > lvl) {
-            let newLevel = client.db.set(`level_${message.author.id}`,level);
-            message.channel.send(`:tada: ${message.author.toString()}, Level atladın yeni levelin ${newLevel}!`);
-        }
-        client.cooldown.set(`${message.author.id}`, Date.now());
-    }
-}
-
-  //-------------------SEVİYE SİSTEMİ-------------------\\
-
-  
 //--------------------ETİKET PREFİX--------------------\\
 
 client.on('message', message => {
