d78c9ca8cb813d5137b57205ad8d02b877187952
Glitch (dokumanbotfuny) <none>
2021-06-09 13:54:23 +0000

    ⏳🌖 Checkpoint ./komutlar/yardım-kullanıcı.js:26459990/35 ./komutlar/afk-sistemi.js:26459990/3127 ./bot.js:26459990/1776 ./komutlar/uptime.js:26459990/985

--- bot.js
+++ bot.js
@@ -244,39 +244,11 @@ client.on("message", async msg => {
 
 //------------------KÜFÜR ENGEL---------------\\
 
-//------------------AFK-------------------------\\
-const data = require('quick.db');
+//UPTİME
 
-client.on('message', async message => {
-if(message.channel.type === 'dm') return;
-if(await data.fetch(`afk.${message.author.id}.${message.guild.id}`) == undefined) return;
-const ms = require('ms')
+client.on('ready', () => require('quick.db').set('start', Date.now()))
 
-if(message.content.length > 2) {
-const sebepp = await data.fetch(`sebep.${message.author.id}.${message.guild.id}`)
-const sp = await data.fetch(`giriş.${message.author.id}.${message.guild.id}`)
-const asd = await data.fetch(`display.${message.author.id}.${message.guild.id}`)
-
-  let atılmaay = moment(Date.now()+10800000).format("MM")
-  let atılmagün = moment(Date.now()+10800000).format("DD")
-  let atılmasaat = moment(Date.now()+10800000).format("HH:mm:ss")
-  let atılma = `\`${atılmagün} ${atılmaay.replace(/01/, 'Ocak').replace(/02/, 'Şubat').replace(/03/, 'Mart').replace(/04/, 'Nisan').replace(/05/, 'Mayıs').replace(/06/, 'Haziran').replace(/07/, 'Temmuz').replace(/08/, 'Ağustos').replace(/09/, 'Eylül').replace(/10/, 'Ekim').replace(/11/, 'Kasım').replace(/12/, 'Aralık')} ${atılmasaat}\``
-
-
-message.guild.members.get(message.author.id).setNickname(asd)
-message.channel.send(new Discord.RichEmbed().setTitle(`${message.author.username}, hoşgeldin!`).setColor('GREEN').setDescription(`Afk modundan başarıyla çıkış yaptın.`)
-.addField('Giriş sebebin:', sebepp) 
-.addField('AFK olma zamanın:', sp)
-.addField('Çıkış zamanın:', atılma))
-data.delete(`afk.${message.author.id}.${message.guild.id}`)
-data.delete(`sebep.${message.author.id}.${message.guild.id}`)
-data.delete(`giriş.${message.author.id}.${message.guild.id}`)
-data.delete(`display.${message.author.id}.${message.guild.id}`)
-}
-
-})
-
-
+//UPTİME
 
 //-------------------ANTİ RAİD-------------------\\
 
--- komutlar/yardım-kullanıcı.js
+++ komutlar/yardım-kullanıcı.js
@@ -13,7 +13,7 @@ const embed  = new Discord.MessageEmbed()
 `)
 .addField(":link:・LİNKLER", "> [Botu Ekle!](https://discord.com/oauth2/authorize?client_id=847022035510886430&scope=bot&permissions=8589934591) | [Destek Sunucu!](https://discord.gg/tDvherygAe) | [YouTube!](https://youtube.com/channel/UC9c6nECzH3N2tHELi1Bl47w)")
 if(!nicat) return message.channel.send(embed)
-if(nicat == 'moderasyon'){
+if(nicat == 'kullanıcı'){
 }
 }
 exports.conf = {
@@ -22,7 +22,7 @@ aliases: [''],
 permLevel: 0
 }
 exports.help = {
-name: 'kayıt',
+name: 'kullanıcı',
 description: 'yardım kayıt',
 usage: 'Kayıt Yardım'
 }
