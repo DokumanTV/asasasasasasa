a2d762a3ce9d998227b18e0b10dbe5ddf8275cab
Glitch (dokumanbotfuny) <none>
2021-06-09 13:42:47 +0000

    🌅😼 Checkpoint ./komutlar/afk-sistemi.js:26459990/1610 ./package.json:26459990/2315 ./bot.js:26459990/1782 ./komutlar/yardım-kullanıcı.js:26459990/2520 ./komutlar/yardımmenü.js:26459990/160

---
+++ komutlar/yardım-kullanıcı.js
@@ -0,0 +1,28 @@
+const ayarlar = require('../ayarlar.json')
+const Discord = require('discord.js')
+exports.run = async (client,message,args,db) => {
+
+var p = ayarlar.prefix;
+let nicat = args[0]
+
+const embed  = new Discord.MessageEmbed()
+.setTitle(':boy: Kullanıcı Yardım Komutları :boy:')
+.setColor('0x36393e')
+.setDescription(`
+\**${p}afk <sebep>\** - Afk Moduna Geçersiniz
+`)
+.addField(":link:・LİNKLER", "> [Botu Ekle!](https://discord.com/oauth2/authorize?client_id=847022035510886430&scope=bot&permissions=8589934591) | [Destek Sunucu!](https://discord.gg/tDvherygAe) | [YouTube!](https://youtube.com/channel/UC9c6nECzH3N2tHELi1Bl47w)")
+if(!nicat) return message.channel.send(embed)
+if(nicat == 'moderasyon'){
+}
+}
+exports.conf = {
+enabled: true,
+aliases: [''],
+permLevel: 0
+}
+exports.help = {
+name: 'kayıt',
+description: 'yardım kayıt',
+usage: 'Kayıt Yardım'
+}
