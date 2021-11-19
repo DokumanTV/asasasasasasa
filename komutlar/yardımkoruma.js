6adc5b056b6d85308d183fd0c333155bdd63e281
Glitch (plutomben) <none>
2021-11-19 15:55:07 +0000

    🏊🏼🌂 Checkpoint ./bot.js:26459990/11011 ./komutlar/yardımkoruma.js:26459990/45 ./ayarlar.json:26459990/35

--- komutlar/yardımkoruma.js
+++ komutlar/yardımkoruma.js
@@ -11,7 +11,6 @@ const embed  = new Discord.MessageEmbed()
 .setColor('0x36393e')
 .setDescription(`
 \**${p}küfürengel \** - Küfür Engel Açar..
-\**${p}antispam \** - KSpam Korumasını Açar.
 `)
 .addField(":link:・LİNKLER", "> [Botu Ekle!](https://discord.com/oauth2/authorize?client_id=847022035510886430&scope=bot&permissions=8589934591) | [Destek Sunucu!](https://discord.gg/Jycf7FEZAa) | [YouTube!](https://youtube.com/channel/UC9c6nECzH3N2tHELi1Bl47w)")
 .setImage("https://cdn.discordapp.com/attachments/910121358669783040/910121456741007400/standard.gif")
