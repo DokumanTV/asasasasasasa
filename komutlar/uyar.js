a4f056a6a6325f81a37fd2ad27562952d4ac881b
Glitch (rowan-spice-amber) <none>
2021-11-19 15:04:24 +0000

    ⚓️🦃 Checkpoint ./komutlar/uyar.js:26459990/14425

--- komutlar/uyar.js
+++ komutlar/uyar.js
@@ -3,44 +3,44 @@ const fs = require("fs");
 const db = require("croxydb");
 const generator = require("generate-password");
 
-exports.run = (client, message, args) => {
-  if (!message.member.hasPermission("MANAGE_MESSAGES"))
-    return message.reply(
+exports.run = (client, msg, args) => {
+  if (!msg.member.hasPermission("MANAGE_MESSAGES"))
+    return msg.reply(
       `Bu Komutu Kullanabilmek İçin \`Mesajları Yönet\` İznine Sahip Olmalısın!`
     );
 
-  let user = message.mentions.members.first();
+  let user = msg.mentions.members.first();
   let reason = args.slice(1).join(" ");
 
-  if (!user) return message.reply("**Uyaracağın Kişiyi Etiketlemelisin!**");
-  if (!reason) return message.reply("**Uyarma Sebebini Yazmalısın!**");
-  if (user.id === message.author.id)
-    return message.reply("**Kendini Uyaramazsın!**");
-  if (user.user.bot) return message.reply("**Botları Uyaramazsın!**");
+  if (!user) return msg.reply("**Uyaracağın Kişiyi Etiketlemelisin!**");
+  if (!reason) return msg.reply("**Uyarma Sebebini Yazmalısın!**");
+  if (user.id === msg.author.id)
+    return msg.reply("**Kendini Uyaramazsın!**");
+  if (user.user.bot) return msg.reply("**Botları Uyaramazsın!**");
 
   if (
-    message.guild.members.get(user.id).highestRole.calculatedPosition >
-    message.member.highestRole.calculatedPosition
+    msg.guild.members.get(user.id).highestRole.calculatedPosition >
+    msg.member.highestRole.calculatedPosition
   )
-    return message.channel.send(
+    return msg.channel.send(
       `**Bu Kişinin Rolü Senin Rolünden Daha Yüksek!**`
     );
 
   
     user.send(
-      `<@${user.id}>, \n**${message.guild.name}** Adlı Sunucuda **${reason}** Sebebi İle Uyarıldın! \nKuralları Çiğnemeye Devam Eder İsen Susturulabilir, Atılabilir Veya Yasaklanabilirsin!`
+      `<@${user.id}>, \n**${msg.guild.name}** Adlı Sunucuda **${reason}** Sebebi İle Uyarıldın! \nKuralları Çiğnemeye Devam Eder İsen Susturulabilir, Atılabilir Veya Yasaklanabilirsin!`
     );
 
     const cse = new Discord.RichEmbed()
       .setTitle("Code Share Uyarı Sistemi")
       .setColor("BLUE")
-    .setThumbnail(message.guild.iconURL)
+    .setThumbnail(msg.guild.iconURL)
       .setDescription(
         `<@${user.id}> Adlı Kullanıcı **${reason}** Sebebi İle Başarıyla Uyarıldı!`
       )
       .setTimestamp()
       .setFooter("Made By. Code Share");
-    message.channel.send(cse);
+    msg.channel.send(cse);
   
   db.add(`uyarılar_${user.id}`, 1);
 
@@ -54,20 +54,20 @@ exports.run = (client, message, args) => {
   let komutlar = JSON.parse(fs.readFileSync("./uyarılar.json", "utf8"));
   var altkomut = "";
 
-  if (komutlar[message.guild.id + "-" + user.id]) {
+  if (komutlar[msg.guild.id + "-" + user.id]) {
     for (
       var i = 0;
-      i < Object.keys(komutlar[message.guild.id + "-" + user.id]).length;
+      i < Object.keys(komutlar[msg.guild.id + "-" + user.id]).length;
       i++
     ) {
       if (
         password ===
-        Object.keys(komutlar[message.guild.id + "-" + user.id][i]).toString()
+        Object.keys(komutlar[msg.guild.id + "-" + user.id][i]).toString()
       ) {
         array.push(
           JSON.parse(
             `{"${
-              Object.keys(komutlar[message.guild.id + "-" + user.id][i])[0]
+              Object.keys(komutlar[msg.guild.id + "-" + user.id][i])[0]
             }": "${reason}"}`
           )
         );
@@ -75,22 +75,22 @@ exports.run = (client, message, args) => {
         array.push(
           JSON.parse(
             `{"${
-              Object.keys(komutlar[message.guild.id + "-" + user.id][i])[0]
+              Object.keys(komutlar[msg.guild.id + "-" + user.id][i])[0]
             }": "${
-              komutlar[message.guild.id + "-" + user.id][i][
-                Object.keys(komutlar[message.guild.id + "-" + user.id][i])
+              komutlar[msg.guild.id + "-" + user.id][i][
+                Object.keys(komutlar[msg.guild.id + "-" + user.id][i])
               ]
             }"}`
           )
-        );
+)
       }
       kontrol2.push(
-        Object.keys(komutlar[message.guild.id + "-" + user.id][i])[0].toString()
+        Object.keys(komutlar[msg.guild.id + "-" + user.id][i])[0].toString()
       );
     }
     if (!kontrol2.includes(password)) {
       array.push(JSON.parse(`{"${password}": "${reason}"}`));
-      komutlar[message.guild.id + "-" + user.id] = array;
+      komutlar[msg.guild.id + "-" + user.id] = array;
 
       fs.writeFile("./uyarılar.json", JSON.stringify(komutlar), err => {
         console.log(err);
@@ -98,7 +98,7 @@ exports.run = (client, message, args) => {
 
       return;
     } else {
-      komutlar[message.guild.id + "-" + user.id] = array;
+      komutlar[msg.guild.id + "-" + user.id] = array;
       fs.writeFile("./uyarılar.json", JSON.stringify(komutlar), err => {
         
         console.log(err);
@@ -107,7 +107,7 @@ exports.run = (client, message, args) => {
     }
   } else {
     array.push(JSON.parse(`{"${password}": "${reason}"}`));
-    komutlar[message.guild.id + "-" + user.id] = array;
+    komutlar[msg.guild.id + "-" + user.id] = array;
     fs.writeFile("./uyarılar.json", JSON.stringify(komutlar), err => {
       console.log(err);
     });
