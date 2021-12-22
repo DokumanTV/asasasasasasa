65e8aac40546845e69837f09b940bd96b0f8adc0
Glitch (plutomben) <none>
2021-12-22 15:47:26 +0000

    🌝🍗 Checkpoint ./komutlar/istatistik.js:26459990/675 ./komutlar/mute.js:26459990/3618 ./komutlar/muterole.js:26459990/2000

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -8,16 +8,16 @@ exports.run = async (bot, message, args) => {
   const duration = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
   const msg = message
 
-   const bunemq = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
-   const annencilermaldır = new Discord.MessageEmbed()
+   const süre = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
+   const mdessage = new Discord.MessageEmbed()
 
   .setColor('RANDOM')
-  .setFooter('GuardMayFe  \'Buyur benim istatistiklerim', bot.user.displayAvatarURL())
+  .setFooter('Pluto+  \' İstatistikler', bot.user.displayAvatarURL())
   .setThumbnail(bot.user.displayAvatarURL({dynamic: true, format: "png", size: 1024}))
-  .addField("» **Botun Sahibi**", "<402047297963294730>| ByMayFe_0#2956  ")
-  .addField("»  **Geliştirici** ","<373493092881530887>| seyfttnbdk_0#3548 ")
+  .addField("» **Botun Sahibi**", "<726482014877777980>| 𝑍𝑜𝑙𝑑𝑖𝑛𝑔 么 𝐵𝑙𝑒𝑎𝑐ℎ𝑒𝑟𝑠#8617 ")
+  .addField("»  **Geliştirici** ","<726482014877777980>| 𝑍𝑜𝑙𝑑𝑖𝑛𝑔 么 𝐵𝑙𝑒𝑎𝑐ℎ𝑒𝑟𝑠#8617 ")
   .addField("» **Bellek kullanımı**", (process.memoryUsage().heapUsed / 512 / 512).toFixed(2) + ' MB', true)  
-  .addField("» **Çalışma süresi**", bunemq)
+  .addField("» **Çalışma süresi**", süre)
   .addField('» **Kullanıcılar**:', bot.guilds.cache.reduce((a, b) => a + b.memberCount, 0))
   .addField("» **Sunucular**", bot.guilds.cache.size.toLocaleString(), true)
   .addField("» **Kanallar**", bot.channels.cache.size.toLocaleString(), true)
@@ -27,11 +27,11 @@ exports.run = async (bot, message, args) => {
   .addField("» **CPU**", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
   .addField("» **Bit**", `\`${os.arch()}\``, true)
   .addField("» **İşletim Sistemi**", `\`\`${os.platform()}\`\``)
-  .addField("**» Bot Davet**", " [Davet Et](https://discordapp.com/oauth2/authorize?client_id=596071936799277116&scope=bot&permissions=2146958847)")
-  .addField("**» Destek Sunucusu**", " [Sunucumuza Katıl](https://discord.gg/codare)")
-  .addField("**» Voteleme sayfası**", " [OYLAR MISIN?](https://top.gg/bot/596071936799277116/vote)")
+  .addField("**» Bot Davet**", " [Davet Et](https://discord.com/oauth2/authorize?client_id=847022035510886430&scope=bot&permissions=8589934591)")
+  .addField("**» Destek Sunucusu**", " [Sunucumuza Katıl](https://discord.gg/Jycf7FEZAa)")
+  .addField("**» Voteleme sayfası**", " [Oy Ver?](https://top.gg/bot/847022035510886430/vote)")
 
- return message.channel.send(annencilermaldır);
+ return message.channel.send(mdessage);
   };
  
 exports.conf = {
