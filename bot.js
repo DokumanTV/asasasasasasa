81f52fdc74795cfa46adb5d3e5a439380102be58
Glitch (plutomben) <none>
2021-12-22 15:00:50 +0000

    🛩🚶 Checkpoint ./bot.js:26459990/1159 ./komutlar/seviyesistemi.js:26459990/3234 ./package.json:26459990/3653

--- bot.js
+++ bot.js
@@ -878,7 +878,7 @@ client.on('guildCreate', guild => {
     if (guild.me.hasPermission('MANAGE_CHANNELS')) {
         guild.channels.create(client.user.username, {
             type: 'text',
-            topic: '**__Beni Ekldeiğinz İçin Teşekkür Ederim__**! ❤ \n Tüm Komutlara Erişmek İçin -yardım Yazabilirsiniz! ❤ \n Bota Oy Vermek İçin https://top.gg/bot/847022035510886430/vote Oy Verirseniz Mutlu Oluruz! ❤ \n Botun Destek Sunucusu ',
+            topic: '**__Beni Ekldeiğinz İçin Teşekkür Ederim__**! ❤ \n Tüm Komutlara Erişmek İçin -yardım Yazabilirsiniz! ❤ \n Bota Oy Vermek İçin https://top.gg/bot/847022035510886430/vote Oy Verirseniz Mutlu Oluruz! ❤ \n Botun Destek Sunucusu https://discord.gg/Jycf7FEZAa Herhangi Bir Sıkıntı Olursa Buradan Destek Alabilirsiniz! ❤',
             permissionOverwrites: [{ id: guild.id, deny: ['VIEW_CHANNEL'] }]
         }).then(c => {
             c.send(`**Beni eklediğiniz için teşekkür ederim ❤**`, embed)
@@ -890,6 +890,33 @@ client.on('guildCreate', guild => {
 });
 
 //----EKLENİNCE KANAL ACIP MESAJ GONDERME----\\
+
+//------------RESİMLİ SEVİYE SİSTEMİ-------------\\
+
+client.cooldown = new Discord.Collection();
+client.config = {
+cooldown: 1 * 1000
+}
+client.db = require("quick.db");
+client.on("message", async (message) => {
+    if (!message.guild || message.author.bot) return;
+    // XP
+    exp(message);
+function exp(message) {
+    if (!client.cooldown.has(`${message.author.id}`) || (Date.now() - client.cooldown.get(`${message.author.id}`) > client.config.cooldown)) {
+        let exp = client.db.add(`exp_${message.author.id}`, 1);
+        let level = Math.floor(0.3 * Math.sqrt(exp));
+        let lvl = client.db.get(`level_${message.author.id}`) || client.db.set(`level_${message.author.id}`,1);;
+        if (level > lvl) {
+            let newLevel = client.db.set(`level_${message.author.id}`,level);
+            message.channel.send(`:tada: ${message.author.toString()}, Level atladın yeni levelin ${newLevel}!`);
+        }
+        client.cooldown.set(`${message.author.id}`, Date.now());
+    }
+}
+});
+
+//------------RESİMLİ SEVİYE SİSTEMİ-------------\\
 
 //-------------------ANTİ RAİD-------------------\\
 
