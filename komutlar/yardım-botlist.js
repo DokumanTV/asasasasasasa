df01fc881f0ec17f06d9545ba3071824b9030c2a
Glitch (plutomben) <none>
2021-12-24 16:48:49 +0000

    🏟💨 Checkpoint ./komutlar/yardım-botlist.js:26459990/2836 ./komutlar/yardımmenü.js:26459990/103

--- komutlar/yardım-botlist.js
+++ komutlar/yardım-botlist.js
@@ -6,17 +6,40 @@ var p = ayarlar.prefix;
 let nicat = args[0]
 
 const embed  = new Discord.MessageEmbed()
-.setTitle(':boy: Kullanıcı Yardım Komutları :boy:')
+.setTitle('🤖 Botlist Yardım Komutları 🤖')
 .setColor('0x36393e')
 .setDescription(`
-\**${p}afk <sebep>\** - Afk Moduna Geçersiniz.
-\**${p}avatar \** - Avatarınızı Atar.
+\**${p}-----------------------**BOT İLE İLGİLİ OLAN KOMUTLAR**---------------------------
+\**${p}bot-ekleme-log sıfırla\** - Üyenin Hangi kanalda botu ekleyeni kapatır!
+\**${p}botlog #kanal\** - Botun reddedilip onaylanan bekliyenleri gözüken logu ayarlar!
+\**${p}botlog sıfırla\** - Botun reddedilip onaylanan bekliyenleri gözüken logu sıfırlar!
+\**${p}botlist-yetkili-rol\** - Botlistte botu onaylıcak reddedicek rolü ayarlar!
+\**${p}botlist-yetkili-rol sıfırla\** - Botları onaylıcak reddedicek rolü sıfırlar
+\**${p}yetkili-log #kanal\** - Botlist yetkili log kanal ayarlar
+\**${p}yetkili-log sıfırla\** - Botlist Yetkili Log kanalı sıfırlar
+\**${p}botekle <id> <prefix>\** - Üye botunu ekliyebilir
+\**${p}botekle-üye-rol\** - Botu Onaylayınca botun sahibine vericek rolü ayarlar
+\**${p}botekle-üye-rol sıfırla\** - Botu Onaylayınca botun sahibine vericek rolü sıfırlar
+\**${p}botekle-bot-rol\** - Botu Onaylayınca botun kendisine vericek rolü ayarlar
+\**${p}botekle-bot-rol sıfırla\** - Botu Onaylayınca botun kendisine vericek rolü sıfırlar
+\**${p}---------------------**SERTİFİKA İLE İLGİLİ OLAN KOMUTLAR**------------------------
+\**${p}sertifika\** - Botunuza Sertifika basvurusu yaparsınız
+\**${p}sertifika-onay <botsahipid> <botid>\** - Botu Onaylanan Kişinin Sertifika Başvurusunu Onaylar
+\**${p}sertifika-reddet <botsahipid> <botid> <sebeb>\** - Botu Onaylanan Kişinin Sertifika Başvurusunu Reddeder.
+\**${p}sertifika-üye-rol @\rol\\** - Sertifika onaylayınca bot sahibine verilcek olan rolü ayarlar.
+\**${p}sertifika-üye-rol sıfırla\** - Sertifika onaylayınca bot sahibine verilcek olan rolü sıfırlar.
+\**${p}sertifika-bot-rol @rol\** - Sertifika onaylayınca bot sahibine verilcek olan rolü ayarlar.
+\**${p}sertifika-bot-rol sıfırla\** - Sertifika onaylayınca bot sahibine verilcek olan rolü sıfırlar.
+\**${p}sertifika-ekleme-log #kanal\** - Hangi Kanalda sadece sertifika basvurusu yapılcanı ayarlar.
+\**${p}sertifika-ekleme-log sıfırla\** - Hangi Kanalda sadece sertifika basvurusu yapılcanı sıfırlar.
+\**${p}sertifikalog #kanal\** - Sertifikanın reddedilip onaylandıgının atıcagı logu ayarlar.
+\**${p}sertifikalog sıfırla\** - Sertifikanın reddedilip onaylandıgının atıcagı logu sıfırlar.
 `)
 
 .addField(":link:・LİNKLER", "> [Botu Ekle!](https://discord.com/oauth2/authorize?client_id=847022035510886430&scope=bot&permissions=8589934591) | [Destek Sunucu!](https://discord.gg/Jycf7FEZAa) | [YouTube!](https://youtube.com/channel/UC9c6nECzH3N2tHELi1Bl47w)")
 .setImage("https://cdn.discordapp.com/attachments/910121358669783040/910121456741007400/standard.gif")
 if(!nicat) return message.channel.send(embed)
-if(nicat == 'kullanıcı'){
+if(nicat == 'botlist'){
 }
 }
 exports.conf = {
@@ -25,7 +48,7 @@ aliases: [''],
 permLevel: 0
 }
 exports.help = {
-name: 'kullanıcı',
+name: 'botlist',
 description: 'yardım kayıt',
 usage: 'Kayıt Yardım'
 }
